1c2ec46e5e15a23a60b9c6983a8eaa26d92a5ab0
Paulo Cesar <98140547+iampaylin@users.noreply.github.com>
2024-05-25 12:44:23 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,20 @@
 <?php
-$host = getenv('DB_HOST');
-$db = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
+function parse_database_url($url) {
+    $parsed_url = parse_url($url);
+    
+    return [
+        'host' => $parsed_url['host'],
+        'port' => $parsed_url['port'],
+        'user' => $parsed_url['user'],
+        'pass' => $parsed_url['pass'],
+        'dbname' => ltrim($parsed_url['path'], '/')
+    ];
+}
 
-$conn = new mysqli($host, $user, $pass, $db);
+$database_url = getenv('postgresql://postgres:ZViGntZgSRsLLwwVzZFXAtMMGEeGTSso@roundhouse.proxy.rlwy.net:29734/railway');
+$db_info = parse_database_url($database_url);
+
+$conn = new mysqli($db_info['host'], $db_info['user'], $db_info['pass'], $db_info['dbname'], $db_info['port']);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
