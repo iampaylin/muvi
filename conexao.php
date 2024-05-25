d1ee9581fc505ccb4f25c0a1a9bd045c62b08ea2
Paulo Cesar <98140547+iampaylin@users.noreply.github.com>
2024-05-25 12:34:01 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,12 @@
 <?php
-// Configurações de conexão com o banco de dados
-$servername = "localhost"; // endereço do servidor MySQL
-$username = "root"; // nome de usuário padrão do MySQL no XAMPP
-$password = ""; // senha padrão do MySQL no XAMPP (em branco por padrão)
-$dbname = "muvidatabase"; // nome do seu banco de dados
+$host = getenv('DB_HOST');
+$db = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
 
-// Cria a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db);
 
-// Verifica a conexão
 if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    die("Falha na conexão: " . $conn->connect_error);
 }
 ?>
